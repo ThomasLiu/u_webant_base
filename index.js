036e64c181684410db74dad18216b7bae9650a43
Thomas Lau <thomas_0836@qq.com>
2018-12-18 14:43:19 +0800

    init

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 
 
-import utils from './lib/utils';
-import services from './lib/services';
-import routes from './lib/routes';
-import pages from './lib/pages';
-import layouts from './lib/layouts';
-import config from './lib/config';
+import utils from './lib/baseUtils';
+import services from './lib/baseServices';
+import routes from './lib/baseRoutes';
+import pages from './lib/basePages';
+import layouts from './lib/baseLayouts';
+import config from './lib/baseConfig';
 
 
 module.exports = {
