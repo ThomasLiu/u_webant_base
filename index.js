0d0ac22423c8c5f0e407f01864ce9492d0cbb626
Thomas Lau <thomas_0836@qq.com>
2018-12-18 15:16:15 +0800

    init

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ import utils from './lib/baseUtils';
 import services from './lib/baseServices';
 import config from './lib/baseConfig';
 
-module.exports = {
+export default {
   utils,
   services,
   routes,
