dcdf2fcbba459eb0a152ea7587305ee67ebdc85d
Thomas Lau <thomas_0836@qq.com>
2018-12-19 17:53:53 +0800

    回退到之前版本

--- index.js
+++ index.js
@@ -1,22 +1,9 @@
 const user = require('./lib/model/user')
 const global = require('./lib/model/global')
 const login = require('./lib/model/login')
-const util = require('util_react_web')
-const config = require('umi_base_config')
-const request = require('util_react_request')
-const LS = require('util_storage')
 
 module.exports = {
   user,
   global,
-  login,
-  model: {
-    user,
-    global,
-    login,
-  },
-  util,
-  config,
-  request,
-  LS
+  login
 }
