5eac664dbe322581e6809125c26f15e7c666316d
Thomas Lau <thomas_0836@qq.com>
2018-12-19 15:08:31 +0800

    add login model

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const user = require('./lib/model/user')
 const global = require('./lib/model/global')
+const login = require('./lib/model/login')
 
 module.exports = {
   user,
-  global
+  global,
+  login
 }
